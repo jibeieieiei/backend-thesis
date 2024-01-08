49e442060153af5134ae0ccddd1c1e261e46bae4
thepju <jib.thepjutha@gmail.com>
2024-01-08 17:49:32 +0700

    Arima model done (except: TLI)

--- arima/arima_model.py
+++ arima/arima_model.py
@@ -1,16 +1,18 @@
 import itertools
-import os
-import sys
+import time
 import warnings
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from pandas.plotting import lag_plot
 from sklearn.metrics import mean_squared_error
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from sqlalchemy import create_engine
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.stattools import acf, adfuller, pacf
+
+# Plot Import
+# import matplotlib.pyplot as plt
+# from pandas.plotting import lag_plot
+# from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 warnings.filterwarnings("ignore")
 
@@ -21,32 +23,37 @@ stocks = [
     'EA', 'EGCO', 'GLOBAL', 'GPSC', 'GULF', 'HMPRO', 'INTUCH', 'IVL',
     'KBANK', 'KCE', 'KTB', 'KTC', 'LH', 'MINT', 'MTC', 'OR',
     'OSP', 'PTT', 'PTTEP', 'PTTGC', 'RATCH', 'SAWAD', 'SCB',
-    'SCC', 'SCGP', 'TISCO', 'TLI', 'TOP', 'TRUE', 'TTB', 'TU', 'WHA',
+    'SCC', 'SCGP', 'TISCO',
+    # 'TLI',
+    'TOP', 'TRUE', 'TTB', 'TU', 'WHA',
 ]
 
-
-tf = ["15t"]
+timeframe = ["15t", "1h", "4h", "1d"]
 
 
 class ARIMACalculatorParams:
 
-    def __init__(self, stock_symbol, timeframe):
-        self.stock_symbol = stock_symbol
+    def __init__(self, symbol, timeframe, column):
+        self.symbol = symbol
         self.timeframe = timeframe
+        self.column = column
         self.data = None
+        self.predict_length = 10
         self.load_data()
         self.d, self.p, self.q = -1, -1, -1
+        self.train_test_length = (252, 108)
         self.find_d_value()
         self.find_p_value()
         self.find_q_value()
+        self.check_value()
 
     def load_data(self):
-        file_path = f"../data/{self.timeframe}/{self.stock_symbol}_SET.csv"
+        file_path = f"./data/{self.timeframe}/{self.symbol}_SET.csv"
         self.data = pd.read_csv(file_path)
 
     def find_d_value(self):
         tmp_df = self.data.copy()
-        close_df = tmp_df['Close']
+        close_df = tmp_df[self.column]
         p_val = adfuller(close_df.dropna(), autolag='AIC')[1]
         self.d = 0
         while p_val >= 0.05:
@@ -55,14 +62,14 @@ class ARIMACalculatorParams:
             p_val = adf_res[1]
             self.d += 1
             # close_df.plot(color='green', figsize=(15, 4))
-            # plt.legend([f'{self.stock_symbol} Stock price (diff)'])
-            # plt.title(f'{self.stock_symbol} Stock values (diff)')
+            # plt.legend([f'{self.symbol} Stock price (diff)'])
+            # plt.title(f'{self.symbol} Stock values (diff)')
             # plt.show()
         return self.d
 
     def find_p_value(self):
         tmp_df = self.data.copy()
-        close_df = tmp_df['Close']
+        close_df = tmp_df[self.column]
         # plt.rcParams.update({'figure.figsize': (10, 4)})
         # plot_pacf(close_df.dropna(), method='ols')
         df_pacf = pacf(close_df.dropna(), method='ols')
@@ -75,7 +82,7 @@ class ARIMACalculatorParams:
 
     def find_q_value(self):
         tmp_df = self.data.copy()
-        close_df = tmp_df['Close']
+        close_df = tmp_df[self.column]
         # plt.rcParams.update({'figure.figsize':(10,4)})
         # plot_acf(close_df, fft = True)
         df_acf = acf(close_df, fft=True)
@@ -88,70 +95,153 @@ class ARIMACalculatorParams:
         # When Q Cannot Find -> Using Grid Search
 
     def check_value(self):
+
+        # Volume Handle
+        if self.column == "Volume":
+            self.p, self.d, self.q = (1, 1, 1)
+            print(f"PDQ Param Done {self.symbol}:{self.timeframe}")
+            return
+
         p = range(0, 3) if self.p == -1 else range(self.p, self.p+1)
         d = range(0, 3) if self.d == -1 else range(self.d, self.d+1)
         q = range(0, 3) if self.q == -1 else range(self.q, self.q+1)
 
         pdq = list(itertools.product(p, d, q))
-        # print(pdq)
 
-        last_aic = 999999
+        last_aic = 9999999
         param_pdq = ()
 
         tmp_df = self.data.copy()
-        close_df = tmp_df['Close']
+        close_df = tmp_df[self.column]
 
         for param in pdq:
             model = ARIMA(close_df.dropna(), order=param)
             results = model.fit()
-            print('Order = {}'.format(param))
-            print('AIC = {}'.format(results.aic))
             if results.aic < last_aic:
                 last_aic = results.aic
                 param_pdq = param
-        return param_pdq
 
-    def train_data(self):
-        # partition_data_prediction = {}
-        # partition_data_mse = {}
-        # print(f"Timeframe = {tf}, SYMBOL = {symbol}..............")
-        # df= pd.read_csv(f"./data/{tf}/{symbol}_SET.csv")
-        # if tf == "15t":
-        #     train_data = df[(df['datetime'] > '2023-03-01') & (df['datetime'] < '2023-10-01')]
-        #     training_data = train_data['Close'].values
-        #     test_data = df[(df['datetime'] >= '2023-10-01')]
-        #     test_data = test_data['Close'].values
-        # history = [x for x in training_data]
-        # model_predictions = []
-        # _prediction = []
-        # N_test_observations = len(test_data)
-        # for time_point in range(N_test_observations):
-        #     # print(f'round ={time_point}')
-        #     model = ARIMA(history, order=(5,2,2))
-        #     model_fit = model.fit()
-        #     output = model_fit.forecast(10)
-        #     yhat = round(output[0],2)
-        #     print("output = ", output)
-        #     model_predictions.append(yhat)
-        #     true_test_value = test_data[time_point]
-        #     history.append(yhat)
-        #     # history.append(round(output[1],2))
-        # MSE_error = mean_squared_error(test_data, model_predictions)
-        # print('Testing Mean Squared Error is {}'.format(MSE_error))
-        # for period_predict in range(0,2):
-        #     model = ARIMA(history, order=(5,2,2))
-        #     model_fit = model.fit()
-        #     output = model_fit.forecast()
-        #     yhat = output[0]
-        #     print("y_hat= ", output)
-        #     _prediction.append(yhat)
-        # partition_data_mse[symbol] = MSE_error
-        # partition_data_prediction[symbol] = _prediction
-        pass
+        self.p, self.d, self.q = param_pdq
+        print(f"PDQ Param Done {self.symbol}:{self.timeframe}")
+
+    def mse_measure(self):
+        print(f"MSE Measure... {self.symbol}:{self.timeframe}")
+        tmp_df = self.data.copy()
+        close_df = tmp_df[self.column]
+
+        # Check Data has Enough Length
+        max_length = self.train_test_length[0]+self.train_test_length[1]
+        if len(close_df) < max_length:
+            self.train_test_length = (
+                int(len(close_df)*0.7), len(close_df) - int(len(close_df)*0.7))
+        # Select Top 252 For Train 108 For Test (default)
+        train_length, test_length = self.train_test_length
+
+        # Split Train Test
+        close_df_filter = close_df[len(
+            close_df)-(train_length+test_length):len(close_df)]
+        close_df_filter = close_df_filter.reset_index().drop(columns='index')
+
+        # Train Test To List
+        training_data = [x[0] for x in close_df_filter[: train_length].values]
+        test_data = [x[0] for x in close_df_filter[train_length:].values]
+
+        model_predictions = []
+        # Measure Predict Test Length
+        for time_point in range(len(test_data)):
+
+            model = ARIMA(training_data, order=(
+                self.p, self.d, self.q), enforce_stationarity=False)
+            model_fit = model.fit()
+            output = model_fit.forecast(1)
+            yhat = round(output[0], 2)
+            model_predictions.append(yhat)
+            true_test_value = test_data[time_point]
+            training_data.append(true_test_value)
+
+        mse_error = mean_squared_error(test_data, model_predictions)
+        return mse_error, test_data, model_predictions
+
+    def forecasting(self):
+
+        print(f"Forecasting... {self.symbol}:{self.timeframe}")
+        tmp_df = self.data.copy()
+        close_df = tmp_df[self.column]
+
+        # Select Top 252 For Train 108 For Test (default)
+        train_length, test_length = self.train_test_length
+        # Split Train Test
+        close_df_filter = close_df[len(
+            close_df)-(train_length+test_length):len(close_df)]
+        close_df_filter = close_df_filter.reset_index().drop(columns='index')
+
+        training_data = [x[0] for x in close_df_filter.values]
+
+        # Forecasting
+        prediction = []
+        # self.predict_length for Forecast
+        for _round in range(self.predict_length):
+            model = ARIMA(training_data, order=(self.p, self.d, self.q))
+            model_fit = model.fit()
+            output = model_fit.forecast(1)
+            yhat = output[0]
+            prediction.append(round(yhat, 4))
+
+        return prediction
 
 
-for stock_symbol in stocks[1:2]:
-    stock_analysis = ARIMACalculatorParams(stock_symbol, tf[0])
-    print(stock_analysis.d, stock_analysis.p, stock_analysis.q)
-    print(stock_analysis.check_value())
-    # print(f"D value for {stock_symbol}: {d_value}")
+if __name__ == "__main__":
+
+    start = time.time()
+
+    engine = create_engine('sqlite:///./project.db', echo=False)
+
+    # Train Model
+    partition_data_mse = {}
+    forecasting_length = 10
+    columns = ["Close", "Volume"]
+    for tf in timeframe[:]:
+        mse_each_col = {}
+        for col in columns[:]:
+            mse_each_symbol = {}
+            data_each_symbol = {}
+            test_data = {}
+            model_predictions = {}
+            for symbol in stocks[:]:
+                model = ARIMACalculatorParams(symbol, tf, col)
+                # Add MSE to each_symbol
+                model_mse = model.mse_measure()
+                mse_each_symbol[symbol] = model_mse[0]
+                # Backtesting
+                test_data[symbol+"_test"] = model_mse[1]
+                model_predictions[symbol+"_predict"] = model_mse[2]
+                # Add Data to each_symbol
+                data_each_symbol[symbol] = model.forecasting()
+            # Add Predict Price to DB with col and tf
+            price_df = pd.DataFrame(data=data_each_symbol)
+            price_df.to_sql(
+                name=f'arima_{col.lower()}_{tf}', con=engine,
+                if_exists='replace')
+            # Add Backtesting Price to DB with col and tf
+            test_df = pd.DataFrame(data=test_data)
+            model_predict_df = pd.DataFrame(data=model_predictions)
+            backtest_df = pd.concat([test_df, model_predict_df], axis=1)
+            backtest_df.to_sql(name=f'arima_{col.lower()}_backtest_{tf}',
+                               con=engine,
+                               if_exists='replace')
+            # Add MSE to each Columns
+            mse_each_col[col] = mse_each_symbol
+        # Add MSE to each Timeframe
+        partition_data_mse[tf] = mse_each_col
+
+        # Add MSE Error to DB
+        mse_df = pd.DataFrame(mse_each_col).T.drop(
+            index="Volume").reset_index().drop(columns="index")
+        mse_df.to_sql(
+            name=f'arima_mse_{tf}', con=engine, if_exists='replace')
+    # mse_df = pd.DataFrame(index=[0], data=partition_data_mse)
+
+    end = time.time()
+
+    print(f"Done in {end-start}")
+    # mse_df.to_sql(name='mse_arima', con=engine, if_exists='replace')
---
+++ models.py
@@ -0,0 +1,319 @@
+# import datetime
+
+from sqlalchemy import Column, Float, Integer
+
+from database import Base
+
+# from sqlalchemy.dialects.sqlite import DATETIME
+
+
+class SET50Base():
+    ADVANC = Column(Float)
+    AOT = Column(Float)
+    AWC = Column(Float)
+    BANPU = Column(Float)
+    BBL = Column(Float)
+    BDMS = Column(Float)
+    BEM = Column(Float)
+    BGRIM = Column(Float)
+    BH = Column(Float)
+    BTS = Column(Float)
+    CBG = Column(Float)
+    CENTEL = Column(Float)
+    COM7 = Column(Float)
+    CPALL = Column(Float)
+    CPF = Column(Float)
+    CPN = Column(Float)
+    CRC = Column(Float)
+    DELTA = Column(Float)
+    EA = Column(Float)
+    EGCO = Column(Float)
+    GLOBAL = Column(Float)
+    GPSC = Column(Float)
+    GULF = Column(Float)
+    HMPRO = Column(Float)
+    INTUCH = Column(Float)
+    IVL = Column(Float)
+    KBANK = Column(Float)
+    KCE = Column(Float)
+    KTB = Column(Float)
+    KTC = Column(Float)
+    LH = Column(Float)
+    MINT = Column(Float)
+    MTC = Column(Float)
+    OR = Column(Float)
+    OSP = Column(Float)
+    PTT = Column(Float)
+    PTTEP = Column(Float)
+    PTTGC = Column(Float)
+    RATCH = Column(Float)
+    SAWAD = Column(Float)
+    SCB = Column(Float)
+    SCC = Column(Float)
+    SCGP = Column(Float)
+    TISCO = Column(Float)
+    TLI = Column(Float)
+    TOP = Column(Float)
+    TRUE = Column(Float)
+    TTB = Column(Float)
+    TU = Column(Float)
+    WHA = Column(Float)
+
+
+class BacktestBase():
+    ADVANC_test = Column(Float)
+    AOT_test = Column(Float)
+    AWC_test = Column(Float)
+    BANPU_test = Column(Float)
+    BBL_test = Column(Float)
+    BDMS_test = Column(Float)
+    BEM_test = Column(Float)
+    BGRIM_test = Column(Float)
+    BH_test = Column(Float)
+    BTS_test = Column(Float)
+    CBG_test = Column(Float)
+    CENTEL_test = Column(Float)
+    COM7_test = Column(Float)
+    CPALL_test = Column(Float)
+    CPF_test = Column(Float)
+    CPN_test = Column(Float)
+    CRC_test = Column(Float)
+    DELTA_test = Column(Float)
+    EA_test = Column(Float)
+    EGCO_test = Column(Float)
+    GLOBAL_test = Column(Float)
+    GPSC_test = Column(Float)
+    GULF_test = Column(Float)
+    HMPRO_test = Column(Float)
+    INTUCH_test = Column(Float)
+    IVL_test = Column(Float)
+    KBANK_test = Column(Float)
+    KCE_test = Column(Float)
+    KTB_test = Column(Float)
+    KTC_test = Column(Float)
+    LH_test = Column(Float)
+    MINT_test = Column(Float)
+    MTC_test = Column(Float)
+    OR_test = Column(Float)
+    OSP_test = Column(Float)
+    PTT_test = Column(Float)
+    PTTEP_test = Column(Float)
+    PTTGC_test = Column(Float)
+    RATCH_test = Column(Float)
+    SAWAD_test = Column(Float)
+    SCB_test = Column(Float)
+    SCC_test = Column(Float)
+    SCGP_test = Column(Float)
+    TISCO_test = Column(Float)
+    TLI_test = Column(Float)
+    TOP_test = Column(Float)
+    TRUE_test = Column(Float)
+    TTB_test = Column(Float)
+    TU_test = Column(Float)
+    WHA_test = Column(Float)
+    ADVANC_predict = Column(Float)
+    AOT_predict = Column(Float)
+    AWC_predict = Column(Float)
+    BANPU_predict = Column(Float)
+    BBL_predict = Column(Float)
+    BDMS_predict = Column(Float)
+    BEM_predict = Column(Float)
+    BGRIM_predict = Column(Float)
+    BH_predict = Column(Float)
+    BTS_predict = Column(Float)
+    CBG_predict = Column(Float)
+    CENTEL_predict = Column(Float)
+    COM7_predict = Column(Float)
+    CPALL_predict = Column(Float)
+    CPF_predict = Column(Float)
+    CPN_predict = Column(Float)
+    CRC_predict = Column(Float)
+    DELTA_predict = Column(Float)
+    EA_predict = Column(Float)
+    EGCO_predict = Column(Float)
+    GLOBAL_predict = Column(Float)
+    GPSC_predict = Column(Float)
+    GULF_predict = Column(Float)
+    HMPRO_predict = Column(Float)
+    INTUCH_predict = Column(Float)
+    IVL_predict = Column(Float)
+    KBANK_predict = Column(Float)
+    KCE_predict = Column(Float)
+    KTB_predict = Column(Float)
+    KTC_predict = Column(Float)
+    LH_predict = Column(Float)
+    MINT_predict = Column(Float)
+    MTC_predict = Column(Float)
+    OR_predict = Column(Float)
+    OSP_predict = Column(Float)
+    PTT_predict = Column(Float)
+    PTTEP_predict = Column(Float)
+    PTTGC_predict = Column(Float)
+    RATCH_predict = Column(Float)
+    SAWAD_predict = Column(Float)
+    SCB_predict = Column(Float)
+    SCC_predict = Column(Float)
+    SCGP_predict = Column(Float)
+    TISCO_predict = Column(Float)
+    TLI_predict = Column(Float)
+    TOP_predict = Column(Float)
+    TRUE_predict = Column(Float)
+    TTB_predict = Column(Float)
+    TU_predict = Column(Float)
+    WHA_predict = Column(Float)
+
+# class Price15T(SET50Base, Base):
+#     __tablename__ = "merge_close_15t"
+#     id = Column(Integer, primary_key=True)
+#     datetime = Column(DATETIME(
+#         storage_format="%(year)04d-%(month)02d-%(day)02d " +
+#         "%(hour)02d:%(minute)02d:%(second)02d"
+#     ), default=datetime.datetime.utcnow)
+
+#     def __repr__(self):
+#         return f"<Price15T id={self.id} datetime={self.datetime}>"
+
+
+# class Price1D(SET50Base, Base):
+#     __tablename__ = 'merge_close_1dd'
+#     id = Column(Integer, primary_key=True)
+#     datetime = Column(DATETIME(
+#         storage_format="%(year)04d-%(month)02d-%(day)02d " +
+#         "%(hour)02d:%(minute)02d:%(second)02d"
+#     ), default=datetime.datetime.utcnow)
+
+# ---------------- ARIMA Model ----------------
+
+
+class ArimaClose15T(SET50Base, Base):
+    __tablename__ = "arima_close_15t"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<CloseArima15T id={self.id}>"
+
+
+class ArimaClose1H(SET50Base, Base):
+    __tablename__ = "arima_close_1h"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<CloseArima1H id={self.id}>"
+
+
+class ArimaClose4H(SET50Base, Base):
+    __tablename__ = "arima_close_4h"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<CloseArima4H id={self.id}>"
+
+
+class ArimaClose1D(SET50Base, Base):
+    __tablename__ = "arima_close_15d"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<CloseArima1d id={self.id}>"
+
+
+class ArimaVolume15T(SET50Base, Base):
+    __tablename__ = "arima_volume_15t"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaVolume15T id={self.id}>"
+
+
+class ArimaVolume1H(SET50Base, Base):
+    __tablename__ = "arima_volume_1h"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaVolume1H id={self.id}>"
+
+
+class ArimaVolume4H(SET50Base, Base):
+    __tablename__ = "arima_volume_4h"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaVolume4H id={self.id}>"
+
+
+class ArimaVolume1D(SET50Base, Base):
+    __tablename__ = "arima_volume_1d"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaVolume1D id={self.id}>"
+
+# ------ Backtest ------
+
+
+class ArimaCloseBacktest15T(BacktestBase, Base):
+    __tablename__ = "arima_close_backtest_15t"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaBacktestClose15T id={self.id}>"
+
+
+class ArimaCloseBacktest1H(BacktestBase, Base):
+    __tablename__ = "arima_close_backtest_1h"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaBacktestClose1H id={self.id}>"
+
+
+class ArimaCloseBacktest4H(BacktestBase, Base):
+    __tablename__ = "arima_close_backtest_4h"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaBacktestClose4H id={self.id}>"
+
+
+class ArimaCloseBacktest1D(BacktestBase, Base):
+    __tablename__ = "arima_close_backtest_1d"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaBacktestClose1D id={self.id}>"
+
+# ------ MSE ERROR ------
+
+
+class ArimaMSE15T(SET50Base, Base):
+    __tablename__ = "arima_mse_15t"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaMSE15t id={self.id}>"
+
+
+class ArimaMSE1H(SET50Base, Base):
+    __tablename__ = "arima_mse_1h"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaMSE1H id={self.id}>"
+
+
+class ArimaMSE4H(SET50Base, Base):
+    __tablename__ = "arima_mse_4h"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaMSE4H id={self.id}>"
+
+
+class ArimaMSE1D(SET50Base, Base):
+    __tablename__ = "arima_mse_1d"
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<ArimaMSE1D id={self.id}>"
+# class ArimaVolume15T(SET50Base, Base):
+#     pass
